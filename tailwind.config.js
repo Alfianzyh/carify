9b87e4c11163bb77574ffb05244d75442ed77daf
YaXioz <alfianzyh@gmail.com>
2025-07-05 20:40:45 +0700

    chore: configure Tailwind content paths for index.html and src files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,9 @@
-content: ["./index.html", "./src/**/*.{js,jsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
