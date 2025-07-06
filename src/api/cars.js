d06aaf841eee2344d9b15af2c0e9460b3ee788d1
YaXioz <alfianzyh@gmail.com>
2025-07-06 12:29:59 +0700

    adding api

---
+++ src/api/cars.js
@@ -0,0 +1,6 @@
+// src/api/cars.js
+export const dummyCars = [
+  { id: 1, make: 'Toyota', model: 'Avanza', year: 2020, price: 150000000 },
+  { id: 2, make: 'Honda', model: 'Jazz', year: 2019, price: 175000000 },
+  { id: 3, make: 'Suzuki', model: 'Ertiga', year: 2021, price: 160000000 },
+];
