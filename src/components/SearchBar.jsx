92ccb70f39da1a2c521e2443d0161f2ff8f55ac8
YaXioz <alfianzyh@gmail.com>
2025-07-07 22:36:01 +0700

    update search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-
 function SearchBar({ value, onChange }) {
   return (
-    <input
-      type="text"
-      placeholder="Cari mobil..."
-      className="p-2 border w-full mb-4 rounded-md"
-      value={value}
-      onChange={onChange}
-    />
+    <div className="mb-4">
+      <input
+        type="text"
+        placeholder="Cari mobil..."
+        value={value}
+        onChange={onChange}
+        className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
   );
 }
 
